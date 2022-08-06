b24d14556445275e1f3d2ae0322412cf3fbca01d
Kameshwaran Murugan <kameshwaran@crayond.co>
2022-08-06 09:46:52 +0530

    external js file is changed

--- external.js
+++ external.js
@@ -2,6 +2,7 @@ var a =10;
 function btnClickExternal() {
     document.getElementById('welcome').innerHTML ="Hi this is Kamesh";
     document.getElementById('welcome').style.color = "red";
+    document.getElementById('welcome').style.color = "red";
 
     console.log(a);
 }
